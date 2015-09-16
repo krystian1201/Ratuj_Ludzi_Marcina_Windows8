3dc0fb833783a55d05700cbe770227f5d96a8c4d
krystian1201 <krystian1201@gmail.com>
2015-09-16 20:16:07 +0200

    Clean-up of navigation helpers

--- Ratuj Ludzi/MainPage.xaml.cs
+++ Ratuj Ludzi/MainPage.xaml.cs
@@ -7,55 +7,40 @@ using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 using Ratuj_Ludzi.Common;
 
-// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
 namespace Ratuj_Ludzi
 {
-    /// <summary>
-    /// A basic page that provides characteristics common to most applications.
-    /// </summary>
+   
     public sealed partial class MainPage : Page
     {
-        Random random = new Random();
-        DispatcherTimer enemyTimer = new DispatcherTimer();
-        DispatcherTimer targetTimer = new DispatcherTimer();
-        bool humanCaptured = false;
+        #region Private fields
 
-        private NavigationHelper navigationHelper;
-        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private Random _random = new Random();
+        private DispatcherTimer _enemyTimer = new DispatcherTimer();
+        private DispatcherTimer _targetTimer = new DispatcherTimer();
+        private bool _humanCaptured = false;
 
-        /// <summary>
-        /// This can be changed to a strongly typed view model.
-        /// </summary>
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return this.defaultViewModel; }
-        }
+        private NavigationHelper _navigationHelper;
+        private ObservableDictionary _defaultViewModel = new ObservableDictionary();
 
-        /// <summary>
-        /// NavigationHelper is used on each page to aid in navigation and 
-        /// process lifetime management
-        /// </summary>
-        public NavigationHelper NavigationHelper
-        {
-            get { return this.navigationHelper; }
-        }
+        #endregion Private fields
 
 
         public MainPage()
         {
             this.InitializeComponent();
 
-            enemyTimer.Tick += enemyTimer_Tick;
-            enemyTimer.Interval = TimeSpan.FromSeconds(2);
+            _enemyTimer.Tick += enemyTimer_Tick;
+            _enemyTimer.Interval = TimeSpan.FromSeconds(2);
 
-            targetTimer.Tick += targetTimer_Tick;
-            targetTimer.Interval = TimeSpan.FromSeconds(.1);
-            this.navigationHelper = new NavigationHelper(this);
-            this.navigationHelper.LoadState += navigationHelper_LoadState;
-            this.navigationHelper.SaveState += navigationHelper_SaveState;
+            _targetTimer.Tick += targetTimer_Tick;
+            _targetTimer.Interval = TimeSpan.FromSeconds(.1);
+            this._navigationHelper = new NavigationHelper(this);
+            this._navigationHelper.LoadState += navigationHelper_LoadState;
+            this._navigationHelper.SaveState += navigationHelper_SaveState;
         }
-        void targetTimer_Tick(object sender, object e)
+
+        private void targetTimer_Tick(object sender, object e)
         {
             progressBar.Value += 1;
             if (progressBar.Value >= progressBar.Maximum)
@@ -66,18 +51,122 @@ namespace Ratuj_Ludzi
         {
             if (!playArea.Children.Contains(gameOverText))
             {
-                enemyTimer.Stop();
-                targetTimer.Stop();
-                humanCaptured = false;
+                _enemyTimer.Stop();
+                _targetTimer.Stop();
+                _humanCaptured = false;
                 startButton.Visibility = Visibility.Visible;
                 playArea.Children.Add(gameOverText);
             }
         }
 
-        void enemyTimer_Tick(object sender, object e)
+        private void enemyTimer_Tick(object sender, object e)
         {
             AddEnemy();
         }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            human.IsHitTestVisible = true;
+            _humanCaptured = false;
+            progressBar.Value = 0;
+            startButton.Visibility = Visibility.Collapsed;
+            playArea.Children.Clear();
+            playArea.Children.Add(target);
+            playArea.Children.Add(human);
+            _enemyTimer.Start();
+            _targetTimer.Start();
+        }
+
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
+            AnimateEnemy(enemy, _random.Next((int)playArea.ActualHeight - 100), 
+                _random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
+            playArea.Children.Add(enemy);
+
+            enemy.PointerEntered += enemy_PointerEntered;
+        }
+
+        void enemy_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (_humanCaptured)
+                EndTheGame();
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(_random.Next(4, 6)))
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, propertyToAnimate);
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+            
+        }
+
+        private void human_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (_enemyTimer.IsEnabled)
+            {
+                _humanCaptured = true;
+                human.IsHitTestVisible = false;
+            }
+        }
+
+        private void target_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (_targetTimer.IsEnabled && _humanCaptured)
+            {
+                progressBar.Value = 0;
+                Canvas.SetLeft(target, _random.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(target, _random.Next(100, (int)playArea.ActualHeight - 100));
+                Canvas.SetLeft(human, _random.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(target, _random.Next(100, (int)playArea.ActualHeight - 100));
+                _humanCaptured = false;
+                human.IsHitTestVisible = true;
+                
+            }
+        }
+
+        private void playArea_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (_humanCaptured)
+            {
+                Point pointerPosition = e.GetCurrentPoint(null).Position;
+                Point relativePosition = grid.TransformToVisual(playArea).TransformPoint(pointerPosition);
+                if ((Math.Abs(relativePosition.X - Canvas.GetLeft(human)) > human.ActualWidth *3)
+                    || (Math.Abs(relativePosition.Y - Canvas.GetTop(human)) > human.ActualHeight * 3))
+                {
+                    _humanCaptured = false;
+                    human.IsHitTestVisible = true;
+                }
+                else
+                {
+                    Canvas.SetLeft(human, relativePosition.X - human.ActualWidth / 2);
+                    Canvas.SetTop(human, relativePosition.Y - human.ActualHeight / 2);
+                }
+            }
+        }
+
+        private void playArea_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            if (_humanCaptured)
+                EndTheGame();
+        }
+
+        #region NavigationHelper registration
 
         /// <summary>
         /// Populates the page with content passed during navigation. Any saved state is also
@@ -106,7 +195,22 @@ namespace Ratuj_Ludzi
         {
         }
 
-        #region NavigationHelper registration
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this._defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this._navigationHelper; }
+        }
 
         /// The methods provided in this section are simply used to allow
         /// NavigationHelper to respond to the page's navigation methods.
@@ -119,121 +223,14 @@ namespace Ratuj_Ludzi
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            navigationHelper.OnNavigatedTo(e);
+            _navigationHelper.OnNavigatedTo(e);
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
-            navigationHelper.OnNavigatedFrom(e);
+            _navigationHelper.OnNavigatedFrom(e);
         }
 
         #endregion
-
-        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void startButton_Click(object sender, RoutedEventArgs e)
-        {
-            StartGame();
-        }
-
-        private void StartGame()
-        {
-            human.IsHitTestVisible = true;
-            humanCaptured = false;
-            progressBar.Value = 0;
-            startButton.Visibility = Visibility.Collapsed;
-            playArea.Children.Clear();
-            playArea.Children.Add(target);
-            playArea.Children.Add(human);
-            enemyTimer.Start();
-            targetTimer.Start();
-        }
-
-        private void AddEnemy()
-        {
-            ContentControl enemy = new ContentControl();
-            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
-            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
-            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), 
-                random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
-            playArea.Children.Add(enemy);
-
-            enemy.PointerEntered += enemy_PointerEntered;
-        }
-
-        void enemy_PointerEntered(object sender, PointerRoutedEventArgs e)
-        {
-            if (humanCaptured)
-                EndTheGame();
-        }
-
-        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
-        {
-            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
-            DoubleAnimation animation = new DoubleAnimation()
-            {
-                From = from,
-                To = to,
-                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
-            };
-            Storyboard.SetTarget(animation, enemy);
-            Storyboard.SetTargetProperty(animation, propertyToAnimate);
-            storyboard.Children.Add(animation);
-            storyboard.Begin();
-            
-        }
-
-        private void human_PointerPressed(object sender, PointerRoutedEventArgs e)
-        {
-            if (enemyTimer.IsEnabled)
-            {
-                humanCaptured = true;
-                human.IsHitTestVisible = false;
-            }
-        }
-
-        private void target_PointerEntered(object sender, PointerRoutedEventArgs e)
-        {
-            if (targetTimer.IsEnabled && humanCaptured)
-            {
-                progressBar.Value = 0;
-                Canvas.SetLeft(target, random.Next(100, (int)playArea.ActualWidth - 100));
-                Canvas.SetTop(target, random.Next(100, (int)playArea.ActualHeight - 100));
-                Canvas.SetLeft(human, random.Next(100, (int)playArea.ActualWidth - 100));
-                Canvas.SetTop(target, random.Next(100, (int)playArea.ActualHeight - 100));
-                humanCaptured = false;
-                human.IsHitTestVisible = true;
-                
-            }
-        }
-
-        private void playArea_PointerMoved(object sender, PointerRoutedEventArgs e)
-        {
-            if (humanCaptured)
-            {
-                Point pointerPosition = e.GetCurrentPoint(null).Position;
-                Point relativePosition = grid.TransformToVisual(playArea).TransformPoint(pointerPosition);
-                if ((Math.Abs(relativePosition.X - Canvas.GetLeft(human)) > human.ActualWidth *3)
-                    || (Math.Abs(relativePosition.Y - Canvas.GetTop(human)) > human.ActualHeight * 3))
-                {
-                    humanCaptured = false;
-                    human.IsHitTestVisible = true;
-                }
-                else
-                {
-                    Canvas.SetLeft(human, relativePosition.X - human.ActualWidth / 2);
-                    Canvas.SetTop(human, relativePosition.Y - human.ActualHeight / 2);
-                }
-            }
-        }
-
-        private void playArea_PointerExited(object sender, PointerRoutedEventArgs e)
-        {
-            if (humanCaptured)
-                EndTheGame();
-        }
     }
 }
